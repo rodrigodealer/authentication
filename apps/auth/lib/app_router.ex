6da825caf9c7073a9dc72b459f9df09cc76a0d1e
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2015-07-26 20:26:12 -0300

    MySQL query

--- apps/auth/lib/app_router.ex
+++ apps/auth/lib/app_router.ex
@@ -8,8 +8,16 @@ defmodule AppRouter do
   get "/" do
     send_resp(conn, 200, "Hello world")
   end
+  
+  def connection do
+    {:ok, p} = Mariaex.Connection.start_link(hostname: "localhost", username: "root", database: "elixir_test", password: "root")
+    p
+  end
 
   get "/hello" do
+    
+    Mariaex.Connection.query(connection, "INSERT INTO users (name, email, password) VALUES('test', 'bla', 'bla')")
+    
     person = %Person{name: "Meg"}
     map = %{a: 1, b: 2}
     
--- apps/auth/mix.exs
+++ apps/auth/mix.exs
@@ -13,12 +13,13 @@ defmodule RestApi.Mixfile do
   end
 
   def application do
-    [applications: [:logger, :cowboy, :plug, :httpoison]]
+    [applications: [:logger, :cowboy, :plug, :httpoison, :mariaex]]
   end
 
   defp deps do
     [{:cowboy, "~> 1.0.0"}, 
     {:plug, "~> 0.8.2"},
+    {:mariaex, "~> 0.4.2"},
     {:poison,   "~> 1.4.0"},
     {:httpoison, "~> 0.6"}]
   end
